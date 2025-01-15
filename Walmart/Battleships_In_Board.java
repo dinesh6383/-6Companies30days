2d1bd4f66916d11decb78df49ab409994c6135cd
dinesh6383 <dineshravi6383@gmail.com>
2025-01-15 19:47:50 +0530

    Walmart solution

---
+++ Walmart/Battleships_In_Board.java
@@ -0,0 +1,24 @@
+package Walmart;
+
+
+public class Battleships_In_Board {
+    public static int countBattleShips(char[][] board) {
+        int n = board.length, m = board[0].length;
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(board[i][j] == '.') continue;
+                if(i > 0 && board[i - 1][j] == 'X') continue;
+                if(j > 0 && board[i][j - 1] == 'X') continue;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {{'X', '.', '.', '.'}, {'.', 'X', '.', 'X'}, {'X', '.', '.', 'X'}};
+        int result = countBattleShips(board);
+        System.out.println(result);
+    }
+}
