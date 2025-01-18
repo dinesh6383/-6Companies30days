8c89bf98446b02c7d10277bc021a2681ee3e90b9
dinesh6383 <dineshravi6383@gmail.com>
2025-01-18 13:31:44 +0530

    Google solution

---
+++ Google/Query_Kth_Smallest_Trimmed_Number.java
@@ -0,0 +1,45 @@
+package Google;
+
+import java.util.*;
+
+class Paire {
+    String str;
+    int index;
+
+    public Paire(String str, int idx) {
+        this.str = str;
+        this.index = idx;
+    }
+}
+
+public class Query_Kth_Smallest_Trimmed_Number {
+    public static int[] smallestTrimmedNumbers(String[] nums, int[][] queries) {
+        int[] result = new int[queries.length];
+        PriorityQueue<Paire> pq = new PriorityQueue<>((a, b) -> a.str.compareTo(b.str) != 0 ? a.str.compareTo(b.str) : a.index - b.index);
+        
+        for(int i = 0; i < queries.length; i++) {
+            int trim = queries[i][1];
+            int smallest = queries[i][0];
+            for(int j = 0; j < nums.length; j++) {
+                String num = nums[j];
+                pq.add(new Paire(num.substring(num.length() - trim), j));
+            }
+            
+            int index = -1;
+            while(smallest > 0) {
+                index = pq.poll().index;
+                smallest--;
+            }
+            result[i] = index;
+            pq.clear();
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        String[] nums = {"24", "37", "96", "04"};
+        int[][] queries = {{2,1}, {2,2}};
+        int[] result = smallestTrimmedNumbers(nums, queries);
+        System.out.println(Arrays.toString(result));
+    }
+}
