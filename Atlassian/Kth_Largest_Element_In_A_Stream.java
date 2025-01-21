f2ce0a54cbc8e15ce892489c17e02354daae69e4
dinesh6383 <dineshravi6383@gmail.com>
2025-01-21 23:44:15 +0530

    Atlassian solution

---
+++ Atlassian/Kth_Largest_Element_In_A_Stream.java
@@ -0,0 +1,39 @@
+package Atlassian;
+
+import java.util.PriorityQueue;
+
+class kthLargest {
+    int k;
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public kthLargest(int k, int[] nums) {
+        this.k = k;
+        for(int num : nums) {
+            this.add(num);
+        }
+    }
+
+    public int add(int val) {
+        if(pq.size() == k && val <= pq.peek()) return pq.peek();
+        
+        while(pq.size() == k && pq.peek() < val) {
+            pq.poll();
+        }
+        pq.add(val);
+
+        return pq.peek();
+    }
+}
+
+public class Kth_Largest_Element_In_A_Stream {
+    public static void main(String[] args) {
+        kthLargest obj = new kthLargest(3, new int[] {1,1});
+        System.out.println(obj.add(1));
+        System.out.println(obj.add(1));
+        System.out.println(obj.add(3));
+        System.out.println(obj.add(3));
+        System.out.println(obj.add(3));
+        System.out.println(obj.add(4));
+        System.out.println(obj.add(4));
+        System.out.println(obj.add(4));
+    }
+}
